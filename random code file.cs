735c9935797ea5b039ba30ef20b453117e02d0c8
2Doggo4 <itsdoggotime5@gmail.com>
2024-08-20 11:05:01 -0400

    did some stuff

--- random code file.cs
+++ random code file.cs
@@ -1,1 +1,22 @@
-public uhhh;
+using stinky;
+public float uhhh = 0f;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        using (System)
+        {
+            for (int = 1, 10) 
+            {
+                // Does this work?
+                float uhhh += 1f;
+            }
+        }
+;
+    }
+}
+
+public namespace stinky
+{
+    // What do i do here
+}
